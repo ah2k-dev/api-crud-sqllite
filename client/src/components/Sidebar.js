4d23fbdabdc2923d87e47456ed0912e3b3da8315
Ahmed Hassan <ah2k.dev@gmail.com>
2023-04-08 00:02:18 +0500

    Sidebar updates

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
-import { Menu } from "antd";
+import React, { useEffect, useState } from "react";
+import { Button, Col, Input, Menu, Row, Typography } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchDatabaseList } from "../redux/actions/databaseActions";
 
 const Sidebar = () => {
+  const [newdb, setNew] = useState(false);
+  const [dbname, setDbName] = useState("");
   const dispatch = useDispatch();
   const { SubMenu } = Menu;
   const { databases } = useSelector((state) => state.database);
@@ -16,12 +18,54 @@ const Sidebar = () => {
   const handleTableClick = (database, table) => {
     console.log(database, table);
   };
+  const handleAddNew = () => {
+    setNew(true);
+  };
+  const handleDbName = (e) => {
+    setDbName(e.target.value);
+  };
+  const handleCancel = () => {
+    setNew(false);
+  };
   return (
     <div className="sidebar-cont">
-      <div className="sidebar">
+      <div className="sidebar-top">
         <span className="logo">API CRUD SQLITE</span>
       </div>
       <div className="menu">
+        <Row className="list-heading" align='middle' justify='space-between'> 
+          <Col span={18}>
+            <span>Databases List</span>
+          </Col>
+          {!newdb && (
+            <Col span={5}>
+              <Button type="primary" size="small" onClick={handleAddNew}>
+                + New
+              </Button>
+            </Col>
+          )}
+        </Row>
+        {newdb && (
+          <div className="new-database-comp">
+            <span>Create new database</span>
+            <Row justify="end" align="middle">
+              <Col span={24}>
+                <Input
+                  placeholder="Enter database name"
+                  onChange={handleDbName}
+                />
+              </Col>
+              <Col span={12} className="btn-2comp">
+                <Button type="primary" size="small">
+                  Save
+                </Button>
+                <Button size="small" onClick={handleCancel}>
+                  Cancel
+                </Button>
+              </Col>
+            </Row>
+          </div>
+        )}
         <Menu
           mode="inline"
           defaultSelectedKeys={["1"]}
